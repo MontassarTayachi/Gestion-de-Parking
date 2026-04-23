ed6befbfeb68a4861e134d3bdd33bf5f7967a819
MontassarTayachi <montassartayachi2002@gmail.com>
2026-04-23 20:27:20 +0200

    u

--- gestion_parking_basys3.vhd
+++ gestion_parking_basys3.vhd
@@ -1,5 +1,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 entity gestion_parking_basys3 is
     Port (
@@ -32,26 +33,26 @@ architecture Structural of gestion_parking_basys3 is
     end component;
 
     signal nb_places_dispos_int : STD_LOGIC_VECTOR(3 downto 0);
+    signal refresh_counter      : UNSIGNED(16 downto 0) := (others => '0');
+    signal digit_sel            : STD_LOGIC;
+    signal tens_digit           : STD_LOGIC_VECTOR(3 downto 0);
+    signal units_digit          : STD_LOGIC_VECTOR(3 downto 0);
 
-    function hex_to_7seg(value : STD_LOGIC_VECTOR(3 downto 0)) return STD_LOGIC_VECTOR is
+    -- Segments actifs bas : "1111111" = éteint
+    function dec_to_7seg(value : STD_LOGIC_VECTOR(3 downto 0)) return STD_LOGIC_VECTOR is
     begin
         case value is
-            when "0000" => return "1000000";
-            when "0001" => return "1111001";
-            when "0010" => return "0100100";
-            when "0011" => return "0110000";
-            when "0100" => return "0011001";
-            when "0101" => return "0010010";
-            when "0110" => return "0000010";
-            when "0111" => return "1111000";
-            when "1000" => return "0000000";
-            when "1001" => return "0010000";
-            when "1010" => return "0001000";
-            when "1011" => return "0000011";
-            when "1100" => return "1000110";
-            when "1101" => return "0100001";
-            when "1110" => return "0000110";
-            when others => return "0001110";
+            when "0000" => return "1000000"; -- 0
+            when "0001" => return "1111001"; -- 1
+            when "0010" => return "0100100"; -- 2
+            when "0011" => return "0110000"; -- 3
+            when "0100" => return "0011001"; -- 4
+            when "0101" => return "0010010"; -- 5
+            when "0110" => return "0000010"; -- 6
+            when "0111" => return "1111000"; -- 7
+            when "1000" => return "0000000"; -- 8
+            when "1001" => return "0010000"; -- 9
+            when others => return "1111111"; -- éteint
         end case;
     end function;
 
@@ -69,8 +70,42 @@ begin
             complet          => led_complet
         );
 
-    seg <= hex_to_7seg(nb_places_dispos_int);
-    an  <= "1110";
-    dp  <= '1';
+    -- Diviseur d'horloge : ~760 Hz de rafraîchissement par digit
+    process(clk100mhz)
+    begin
+        if rising_edge(clk100mhz) then
+            refresh_counter <= refresh_counter + 1;
+        end if;
+    end process;
+
+    digit_sel <= refresh_counter(16);
+
+    -- Décomposition décimale : valeurs 0-9 → unité seule ; 10-15 → dizaine=1, unité=valeur-10
+    process(nb_places_dispos_int)
+    begin
+        case nb_places_dispos_int is
+            when "1010" => tens_digit <= "0001"; units_digit <= "0000"; -- 10
+            when "1011" => tens_digit <= "0001"; units_digit <= "0001"; -- 11
+            when "1100" => tens_digit <= "0001"; units_digit <= "0010"; -- 12
+            when "1101" => tens_digit <= "0001"; units_digit <= "0011"; -- 13
+            when "1110" => tens_digit <= "0001"; units_digit <= "0100"; -- 14
+            when "1111" => tens_digit <= "0001"; units_digit <= "0101"; -- 15
+            when others => tens_digit <= "1111"; units_digit <= nb_places_dispos_int; -- 0-9
+        end case;
+    end process;
+
+    -- Multiplexage 7 segments : AN0 = unités, AN1 = dizaines
+    process(digit_sel, tens_digit, units_digit)
+    begin
+        if digit_sel = '0' then
+            seg <= dec_to_7seg(units_digit);
+            an  <= "1110"; -- AN0 actif
+        else
+            seg <= dec_to_7seg(tens_digit); -- "1111" => éteint pour 0-9
+            an  <= "1101"; -- AN1 actif
+        end if;
+    end process;
+
+    dp <= '1';
 
 end Structural;
